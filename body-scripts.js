b1aac844de200bac45968d0d9cbf282e08a1b5e2
M. Ali Arman <mohammadali.arman@gmail.com>
2026-01-14 17:16:45 +0100

    minor revisions on the first page

--- body-scripts.js
+++ body-scripts.js
@@ -50,7 +50,7 @@ Ali Arman - Personal Website
             },
             {
                 title: "Traffic Flow Operations",
-                description: "How traffic flow -motorised and non-mototorised- move and interact in transport network"
+                description: "How traffic flow -motorized and non-mototorized- move and interact in transport network"
             },
             {
                 title: "Behavioral Modeling",
